18d833816a3584426ed7bb55b749c269a74bc2ca
yashu13011 <83954702+yashu13011@users.noreply.github.com>
2022-04-30 10:03:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img src="'+data_uri+'" id="captured_img">'
+    });
+}
+
+console.log('ml5 version: ' , ml5.version);
+classifier = ml5.imageClasifier('https://teachablemachine.withgoogle.com/models/SxWBw9usJ/model.json' , modelLoaded);
